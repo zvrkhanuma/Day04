fa7ccf52429115ab49b64b32b74398dc3b0a6b78
zvrkhanuma <almazalovic@gmail.com>
2021-03-11 20:17:53 +0100

    Added second index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+
+var myRes = 75;
+// Only change code below this line
+
+myRes++;
+
+
+// Only change code abowe this line
+ return myRes;
+}
+ console.log(main());
+
+ module.export = main
