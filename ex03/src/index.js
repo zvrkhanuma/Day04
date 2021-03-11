c66cf90012955a0b69a1b3ac6b5a1aeb8c1037a2
zvrkhanuma <almazalovic@gmail.com>
2021-03-11 20:23:19 +0100

    Added third index.js

---
+++ ex03/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+
+    var myRes = 16;
+    // Only change code below this line
+    
+    myRes--;
+    
+    
+    // Only change code abowe this line
+     return myRes;
+    }
+     console.log(main());
+    
+     module.export = main
