f82921cbd6a0f26ebfd10bde9278eed447e7690d
zvrkhanuma <almazalovic@gmail.com>
2021-03-11 23:31:46 +0100

    Added seventh index.js

---
+++ ex07/src/index.js
@@ -0,0 +1,18 @@
+function main(numOne, numTwo){
+
+    // Only change code below this line
+    var remainder = numOne % numTwo;
+
+
+
+    // Only change code abowe this line
+
+    return remainder;
+
+}
+
+console.log(main(5, 2));
+console.log(main(15, 4));
+console.log(main(102, 25));
+
+module.export = main;
