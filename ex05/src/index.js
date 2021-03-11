e1d54f41489aaa7aee771a20d9015f2b93eb3262
zvrkhanuma <almazalovic@gmail.com>
2021-03-11 23:12:31 +0100

    Added fifth index.js

---
+++ ex05/src/index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo){
+
+    // Only change code below this line
+    var product = numOne *  numTwo;
+
+
+
+    // Only change code abowe this line
+
+    return product;
+
+}
+
+console.log(main(2.5, 2.0));
+console.log(main(5.5, 5.5));
+console.log(main(100.25, 43.20));
+
+module.export = main;
+
