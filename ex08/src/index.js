a8bbe38b68f4dfd5d3baf78efce190dcc9a24ccd
zvrkhanuma <almazalovic@gmail.com>
2021-03-12 01:49:16 +0100

    Added eight index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,24 @@
+function main(){
+
+    // Only change code below this line
+    var myX = 12;
+    var myY = 19;
+    var myZ = 24;
+
+    myX += 3;
+    myY += 17;
+    myZ -= 20;
+
+    // Only change code abowe this line
+
+    return {
+        myX,
+        myY,
+        myZ,
+    };
+}
+
+console.log(main());
+
+
+module.export = main;
