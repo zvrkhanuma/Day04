021b2a7cfd64f77865ac13455a6f2c9d883dc8e4
zvrkhanuma <almazalovic@gmail.com>
2021-03-12 02:02:51 +0100

    Added ninth index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,24 @@
+function main(){
+
+    // Only change code below this line
+    var firstNum = 6;
+    var secondNum = 25;
+    var thirdNum = 5.2;
+
+    firstNum *= 9;
+    secondNum /= 5;
+    thirdNum *= 10;
+
+    // Only change code abowe this line
+
+    return {
+       firstNum,
+       secondNum,
+       thirdNum,
+    };
+}
+
+console.log(main());
+
+
+module.export = main;
